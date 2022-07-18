76fe31f016707ed359a41c6f6f80833ab2c93b98
bleusaphir <basesayabo.yan@gmail.com>
2022-07-18 11:48:17 +0200

    Occ + Grad with tf-explain

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import tensorflow.keras as keras
 import utils
 import os
 import PIL
+from tf_explain.core import GradCAM, OcclusionSensitivity
 import matplotlib.pyplot as plt
 import numpy as np
 # print("Welcome. Select your Folder : ")
@@ -19,18 +20,17 @@ model = keras.models.load_model('models/Last.h5')
 print("Yo j'suis là")
 def testOcc(model):
     # Load the original image
-    img = keras.preprocessing.image.load_img('test/0.jpg')
+    img = keras.preprocessing.image.load_img('test/cancer/0.jpg')
     img = keras.preprocessing.image.img_to_array(img)
 
     patch_size = 4
-    output = utils.get_sensitivity_map(
-        model=model, image=img, class_index=0, patch_size=patch_size
-    )
+    explainer = OcclusionSensitivity()
+    output = explainer.explain(([img], None), patch_size=patch_size, model=model, class_index=1)
 
     utils.save(output, output_dir="results/", output_name='test1.png')
 
 
-#testOcc(model)
+testOcc(model)
 
 
 
@@ -46,4 +46,4 @@ def testLime(model):
   
 
 #%%
-testLime(model)
+#testLime(model)
